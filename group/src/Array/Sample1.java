e83ec8732e8b9dd1e0eebcbb6666fa1a42b72ce5
susu12susu <104517010+susu12susu@users.noreply.github.com>
2022-05-28 13:45:49 -0400

    this is practice exercise.

---
+++ group/src/Array/Sample1.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String nameOfStudent[]=new String[10]; 
+String nameOfStudents[]= {"Aman","b","c","d","e","f","g","h","i","j"};
+	for(String x:nameofStudent) {
+		System.out.print(x +" ");
+	}
+for(int i=0;i<nameOfStudent.length;i++);
+System.out.println(nameOfStudent[i].length +"");
+}
+}
